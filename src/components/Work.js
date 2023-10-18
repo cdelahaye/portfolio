f5edd5932ffb0f2e41b9b8f2f663eb32c1f9cdab
cdelahaye <delahaye.clara@orange.fr>
2023-10-18 16:41:57 +0300

    create the Projects section

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,15 +1,77 @@
 import React from 'react'
-import { Text, GlobalStyle, Blank, Info } from './Elements'
+import { Text, GlobalStyle, Blank } from './Elements'
+import './work.css'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { Icons } from './Elements'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { projectsData } from '../data/works'
+
+
+
+const WorkItem = (prop) => {
+
+    const tags = prop.tags.map(t => (
+        <div key={t} className='tags'> 
+            {t}  
+        </div>
+    ))
+
+    return (
+        <div className='work-item'>
+
+
+            <div className='date'>
+                {prop.date}
+            </div>
+
+
+            <div className='title'>
+                {prop.title}   
+            </div>
+
+            <div className='github-link'> 
+                <Icons onClick={() => {
+                    window.open(prop.githubLink, '_blank')
+                }}> <FontAwesomeIcon icon={faGithub}  size='xs' />  </Icons>
+
+            </div>
+
+            <div className='description'>
+                {prop.description}  
+            </div>
+
+            {tags}
+
+        </div>  
+    )
+}
 
 
 const Work = () => {
+
+    const workItems = projectsData.map(p => (
+        <WorkItem key={p.id} title={p.title} githubLink={p.githubLink} description={p.description} tags={p.tags} date={p.date}/>
+    )
+    )
 
     return(
         <div>
             <GlobalStyle />
             <Blank> </Blank>
-            <Info>In progress</Info>
-            <Text> My projects </Text>
+
+            <div className='work-container'>
+                {workItems}
+            </div>
+
+            <br/> <br/> <br/> <br/>
+
+
+            <Text> 
+            Upcoming works: 
+            I am currently working on other projects, that will soon be here.
+            </Text>
+
+
         </div>
     );
  
---
+++ src/data/works.js
@@ -0,0 +1,11 @@
+export const projectsData = [
+    {
+        title: 'Portfolio',
+        description: 'Well, the website you are currently on! A static site presenting my resume, my projects and my contact information.',
+        tags: ['JS', 'HTML/CSS'],
+        githubLink: 'https://github.com/cdelahaye/portfolio',
+        date: '10/23',
+        id: 'Portfolio'
+    },
+];
+
